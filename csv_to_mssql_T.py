e48278b445bbeabc10dbd099390f3f50bc935078
Gary.Leung <gary.leung@supwin.com>
2020-06-24 01:30:35 +0800

    added:import m1

--- csv_to_mssql_T.py
+++ csv_to_mssql_T.py
@@ -12,7 +12,11 @@ def histdata_date_parser(dtstr):
     return datetime.strptime(dtstr, '%Y%m%d %H%M%S%f')
 
 
-def import_csv_to_mssql(path, name, symbol):
+def histdata_date_parser_m1(dtstr):
+    return datetime.strptime(dtstr, '%Y%m%d%H%M')
+
+
+def import_csv_to_mssql_tick(path, name, symbol):
     print(path+"--"+name+"--"+symbol)
     start = time.process_time()
     myzip = ZipFile(path)
@@ -46,6 +50,41 @@ def import_csv_to_mssql(path, name, symbol):
     print(str(end-start))
 
 
+def import_csv_to_mssql_m1(path, name, symbol):
+    print(path+"--"+name+"--"+symbol)
+    start = time.process_time()
+    myzip = ZipFile(path)
+    csv = myzip.open(name+'.csv')
+    # Import CSV
+    data = pd.read_csv(csv, header=None,
+                       names=['symbol', 'time', 'open', 'high', 'low', 'close', 'vol'], date_parser=histdata_date_parser_m1,
+                       parse_dates=[1])
+    df = pd.DataFrame(data, columns=['time', 'open', 'high', 'low', 'close'])
+    print(df.count())
+    insert_list = []
+    conn = pyodbc.connect(config.mssql_constr)
+    cursor = conn.cursor()
+    cursor.fast_executemany = True
+
+    for row in df.itertuples():
+        insert_params = (
+            symbol,
+            row[1],
+            row[2],
+            row[3],
+            row[4],
+            row[5],
+        )
+        insert_list.append(insert_params)
+    cursor.executemany('''
+                    INSERT INTO Prices.dbo.M1 (symbol,time,o, h,l,c)
+                    VALUES (?,?,?,?,?,?)
+                    ''', insert_list)
+    conn.commit()
+    end = time.process_time()
+    print(str(end-start))
+
+
 for symbol in config.symbol_list:
     datetime_start = datetime.strptime(config.start_date, '%Y-%m-%d')
     datetime_end = datetime.strptime(config.end_date, '%Y-%m-%d')
@@ -59,10 +98,11 @@ for symbol in config.symbol_list:
                 if not os.path.isdir(file):
                     if "DAT_ASCII_XAUUSD_T" in file and "zip" in file and config.import_type == "tick":
                         filename_arr = os.path.splitext(file)
-                        import_csv_to_mssql(
+                        import_csv_to_mssql_tick(
                             folder_path+"\\"+file, filename_arr[0], symbol)
-                    if "DAT_ASCII_XAUUSD_T" in file and "zip" in file and config.import_type == "m1":
+                    if "DAT_MS_XAUUSD_M1" in file and "zip" in file and config.import_type == "m1":
                         filename_arr = os.path.splitext(file)
-                        import_csv_to_mssql(
-                            folder_path+"\\"+file, filename_arr[0], symbol)
+                        if len(filename_arr[0]) == 23:
+                            import_csv_to_mssql_m1(
+                                folder_path+"\\"+file, filename_arr[0], symbol)
         cur_datetime = cur_datetime+relativedelta(years=1)
